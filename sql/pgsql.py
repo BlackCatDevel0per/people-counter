4b006ffd72a771a1288a2ab0981cbab271da9b63
BlackCatDevel0per <333blackcat333@mail.ru>
2022-02-10 19:16:07 +0500

    Update file

--- sql/pgsql.py
+++ sql/pgsql.py
@@ -25,14 +25,20 @@ class PGSQL:
 		else:
 			print("uuid get error")
 
-	async def __DBTrabsactionExecute(self, request: str, debug: str = None, exception_text: str = None):
+	async def __DBTrabsactionExecute(self, request: str, debug: str = None, exception_text: str = None, EXTYPE: type = Exception):
 		try:
 			connection = await asyncpg.connect(host=self._host, port=self._port, user=self._user, password=self._password)
 			async with connection.transaction():
 				await connection.execute(request)
 				if debug != None: print(debug)
 
-		except Exception as e:
+
+		#except asyncpg.exceptions.DuplicateTableError:
+		#	#print(f"[INFO] Table \"{name}\" exists!")
+		#	print(f"[INFO] Table already exists!")
+
+		# Other exception
+		except EXTYPE as e:
 			print(e)
 			if exception_text != None: print(exception_text)
 
@@ -41,31 +47,27 @@ class PGSQL:
 			await connection.close()
 
 	async def createTable(self, name: str):
-		try:
-			#print(f"[INFO] UUID: {self._current_uuid}") # OK!
-			#if self._table != None:
-			#	return False
-		
-			await self.__DBTrabsactionExecute(f"""CREATE TABLE {name} \
-											      (\
-											      uuid VARCHAR(38) PRIMARY KEY, \
-											      bus_number VARCHAR(32),\
-											      count INTEGER, \
-											      time VARCHAR(32) \
-											      );""",
-											      None,
-											      f"[WARNING] Create table \"{name}\" error!")
-			print(f"[INFO] Table \"{name}\" created successfully!")
-		except asyncpg.exceptions.DuplicateTableError:
-			print(f"[INFO] Table \"{name}\" exists!")
+		#print(f"[INFO] UUID: {self._current_uuid}") # OK!
+		#if self._table != None:
+		#	return False
+	
+		await self.__DBTrabsactionExecute(f"""CREATE TABLE {name} \
+										      (\
+										      uuid VARCHAR(38) PRIMARY KEY, \
+										      bus_number VARCHAR(32),\
+										      count INTEGER, \
+										      time VARCHAR(32) \
+										      );""",
+										      f"[INFO] Table \"{name}\" created successfully!",
+										      f"[WARNING] Create table \"{name}\" error!")
 
 ##############################
 	async def addUUID(self, uuid: str):
 		await self.__DBTrabsactionExecute(f"""INSERT INTO "{self._table}" \
 								     		  (uuid) VALUES ('{uuid}');\
 								  	 	   """,
-								  	 	   f"[INFO] UUID: \"{uuid}\" added to db successfully!",
-								  	 	   f"[WARNING] Add UUID \"{uuid}\" error!")
+		f"[INFO] UUID: \"{uuid}\" added to db successfully!",
+		f"[WARNING] Add UUID \"{uuid}\" error!")
 
 ###
 	async def setPeopleCount(self, count: int):
@@ -73,24 +75,24 @@ class PGSQL:
 											  SET count = {count} \
 									    	  WHERE uuid = '{self._current_uuid}';
 									 	   """,
-									 	   f"[INFO] People count \"{count}\"->\"{self._current_uuid}\" added to db successfully!",
-									 	   f"[WARNING] Add people count \"{count}\"->\"{self._current_uuid}\" error!")
+ 	   f"[INFO] People count \"{count}\"->\"{self._current_uuid}\" added to db successfully!",
+ 	   f"[WARNING] Add people count \"{count}\"->\"{self._current_uuid}\" error!")
 ###
 	async def setBusNumber(self, number: str):
 		await self.__DBTrabsactionExecute(f"""UPDATE "{self._table}" \
 									  	 	  SET bus_number = '{number}' \
 									  		  WHERE uuid = '{self._current_uuid}';
 									 	   """,
-									 	   f"[INFO] Bus number \"{number}\"->\"{self._current_uuid}\" added to db successfully!",
-									 	   f"[WARNING] Add bus number \"{number}\"->\"{self._current_uuid}\" error!")
+ 	   f"[INFO] Bus number \"{number}\"->\"{self._current_uuid}\" added to db successfully!",
+ 	   f"[WARNING] Add bus number \"{number}\"->\"{self._current_uuid}\" error!")
 ###
 	async def setTime(self, time: str):
 		await self.__DBTrabsactionExecute(f"""UPDATE "{self._table}" \
 									   		  SET time = '{time}' \
 									   		  WHERE uuid = '{self._current_uuid}';
 									 	   """, 
-									 	   f"[INFO] Time \"{time}\"->\"{self._current_uuid}\" added to db successfully!", 
-									 	   f"[WARNING] Add btime \"{time}\"->\"{self._current_uuid}\" error!")
+ 	   f"[INFO] Time \"{time}\"->\"{self._current_uuid}\" added to db successfully!", 
+ 	   f"[WARNING] Add btime \"{time}\"->\"{self._current_uuid}\" error!")
 
 ##############################
 
